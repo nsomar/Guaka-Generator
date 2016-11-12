347163b653437774d292b4bba6bd9cb1de192bf2
Omar Abdelhafith <o.arrabi@me.com>
2016-11-12 14:54:47 +0000

    Initial commit

--- Package.swift
+++ Package.swift
@@ -1,9 +1,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "mytest",
+    name: "guaka-cli",
     dependencies: [
-        .Package(url: "https://github.com/oarrabi/Guaka.git", majorVersion: 0),
-        .Package(url: "https://github.com/oarrabi/ENV.git", majorVersion: 0)
+        .Package(url: "/Users/oabdelhafith/Documents/Mac/Swift/Guaka", majorVersion: 0),
+        .Package(url: "https://github.com/oarrabi/ENV.git", majorVersion: 0),
+        .Package(url: "https://github.com/oarrabi/Runner.git", majorVersion: 0),
+        .Package(url: "/Users/oabdelhafith/Documents/Mac/Swift/File", majorVersion: 0),
     ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,20 +1,13 @@
+//
+//  Execute.swift
+//  CommandLine
+//
+//  Created by Omar Abdelhafith on 30/10/2016.
+//
+//
+
 import Guaka
-import ENV
 
-let p = Command(name: "print",  
-                shortUsage: "print can be used to print stuff", 
-                longUsage: "print is just the commands you need, it print everything to the console") 
-{ flags, args in
-  print(flags["prefix"]!.value!)
-  print(args.joined())
-  print(flags["postfix"]!.value!)
-  
-  if let additional = Env.get("ADDITIONAL") {
-    print(additional)
-  }
-}
+setupCommands()
 
-p.add(flag: Flag(longName: "prefix", value: "", shortName: "p", description: "prints a prefix"))
-p.add(flag: Flag(longName: "postfix", value: "", description: "prints a postfix"))
-
-p.execute()
+rootCommand.execute()
