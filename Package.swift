ac543d6dbc88f2bff0219282d114501225a79abf
Omar Abdelhafith <o.arrabi@me.com>
2016-11-12 15:05:41 +0000

    Updating package.swift

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@ import PackageDescription
 let package = Package(
     name: "guaka-cli",
     dependencies: [
-        .Package(url: "/Users/oabdelhafith/Documents/Mac/Swift/Guaka", majorVersion: 0),
+        .Package(url: "https://github.com/oarrabi/Guaka.git", majorVersion: 0),
         .Package(url: "https://github.com/oarrabi/ENV.git", majorVersion: 0),
         .Package(url: "https://github.com/oarrabi/Runner.git", majorVersion: 0),
-        .Package(url: "/Users/oabdelhafith/Documents/Mac/Swift/File", majorVersion: 0),
+        .Package(url: "https://github.com/oarrabi/File.git", majorVersion: 0),
     ]
 )
