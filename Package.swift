1d3f7e16805df69138107b9864d0bf68b4e4bae7
Omar Abdelhafith <o.arrabi@me.com>
2017-01-09 09:06:30 +0000

    Updating Env URL

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/oarrabi/Guaka.git", majorVersion: 0),
-        .Package(url: "https://github.com/oarrabi/ENV.git", majorVersion: 0),
+        .Package(url: "https://github.com/oarrabi/Env.git", majorVersion: 0),
         .Package(url: "https://github.com/oarrabi/Runner.git", majorVersion: 0),
         .Package(url: "https://github.com/oarrabi/File.git", majorVersion: 0),
     ]
