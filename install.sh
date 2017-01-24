0632f872c409d477bab1636dec3ff3acb009081a
Jose Narvaez <goyox86@gmail.com>
2017-01-24 17:19:45 +0000

    Initial install script.

---
+++ install.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+REPOSITORY="oarrabi/Guaka-Generator"
+
+DEFAULT_OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+if ((1<<32)); then
+  DEFAULT_ARCH="X64"
+else
+  DEFAULT_ARCH="386"
+fi
+OS=${3:-$DEFAULT_OS}
+ARCH=${4:-$DEFAULT_ARCH}
+
+REPOSITORY_OWNER=$(echo $REPOSITORY | cut -f1 -d/)
+REPOSITORY_NAME=$(echo $REPOSITORY | cut -f2 -d/)
+RELEASES_URL="https://api.github.com/repos/$REPOSITORY_OWNER/$REPOSITORY_NAME/releases"
+RELEASES_RESPONSE=$(curl -s "$RELEASES_URL")
+
+DEFAULT_TAG_NAME=$(echo "$RELEASES_RESPONSE" \
+           | grep -m 1 "^    \"tag_name\": " \
+           | sed "s/    \"tag_name\"\: \"//g" \
+           | sed "s/[\",]//g")
+TAG_NAME=${5:-$DEFAULT_TAG_NAME}
+
+FILENAME="$REPOSITORY_NAME-$TAG_NAME-$OS-$ARCH.tar.bz2"
+
+DOWNLOAD_URL="https://github.com/$REPOSITORY/releases/download/$TAG_NAME/$FILENAME"
+
+TEMP_TARBAL="/tmp/$FILENAME"
+if [ -f $TEMP_TARBAL ] ; then
+    rm $TEMP_TARBAL
+fi
+
+curl -sL "$DOWNLOAD_URL" -o $TEMP_TARBAL
+
+BINARY_DEST="$HOME/.$REPOSITORY_NAME/bin"
+mkdir -p $BINARY_DEST
+
+tar -jxf $TEMP_TARBAL -C $BINARY_DEST
+
+echo "Make sure $BINARY_DEST is on your \$PATH"
